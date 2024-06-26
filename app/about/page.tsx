0ea00022f33b465858af7a314552bfb0c3a6c6bb
furkanbesirli <79379758+furkanbesirli@users.noreply.github.com>
2024-06-26 11:48:28 +0300

    about2

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -20,19 +20,8 @@ const Page: React.FC = () => {
     <>
       <head>
         <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Omegle is a great place to meet new friends. When you use Omegle, we pick another user at random and let you have a one-on-one chat with each other." />
-        <meta name="keywords" content="Omegle, chat, meet new people, secure chat, online friends" />
-        <meta name="robots" content="index, follow" />
-        <link rel="canonical" href="https://omegle-seven.vercel.app" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
-
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://omegle-seven.vercel.app" />
         <meta property="og:title" content="Omegle.com - Omegle: Talk to strangers!" />
