e2abccd2c2507f44a1ecdc4d064b7ec1e7df04a7
furkanbesirli <79379758+furkanbesirli@users.noreply.github.com>
2024-06-26 13:02:19 +0300

    gtils

--- app/chatWithGirls/page.tsx
+++ app/chatWithGirls/page.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   themeColor: "#ffffff",
 };
 
-const page = () => {
+const Page: React.FC = () => {
   return (
 
 <>
@@ -79,4 +79,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
